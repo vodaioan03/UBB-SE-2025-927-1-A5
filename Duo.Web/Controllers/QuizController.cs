f260f90364f1caeb94cd4473cc0ba0d664d5cb5b
Andreea410 <andreeaungur494@yahoo.com>
2025-05-12 02:11:01 +0300

    Feat: Implemented the quiz page

---
+++ Duo.Web/Controllers/QuizController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Duo.Web.Controllers
+{
+    public class QuizController : Controller
+    {
+        private readonly ILogger<QuizController> _logger;
+
+        public QuizController(ILogger<QuizController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult ViewQuizzes()
+        {
+            return View(); 
+        }
+
+        public IActionResult Index()
+        {
+            return RedirectToAction("ViewQuizzes");
+        }
+
+    }
+}
