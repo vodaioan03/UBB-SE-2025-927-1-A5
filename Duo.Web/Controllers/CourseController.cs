8bc4dc18192284c96e5db5f1cde99beeb9a7b8f6
Suba Dániel <danisuba10@gmail.com>
2025-05-11 23:41:18 +0300

    prototype

---
+++ Duo.Web/Controllers/CourseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Duo.Web.Controllers
+{
+    public class CourseController : Controller
+    {
+        public IActionResult ViewCourses()
+        {
+            return View();
+        }
+    }
+}
