31a37ba116a18e4247434efbfb509d16316d57a6
dosqas <137123562+dosqas@users.noreply.github.com>
2025-05-12 06:30:01 +0300

    Add new exception classes for quiz services
    Introduced `QuizServiceException` and `QuizServiceProxyException` in the `DuoClassLibrary.Exceptions` namespace. Each class extends the base `Exception` class and provides three constructors: a parameterless constructor, one that accepts a message, and another that accepts both a message and an inner exception.

---
+++ DuoClassLibrary/Exceptions/QuizServiceException.cs
@@ -0,0 +1,18 @@
+namespace DuoClassLibrary.Exceptions
+{
+    public class QuizServiceException : Exception
+    {
+        public QuizServiceException()
+        {
+        }
+
+        public QuizServiceException(string message) : base(message)
+        {
+        }
+
+        public QuizServiceException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
---
+++ DuoClassLibrary/Exceptions/QuizServiceProxyException.cs
@@ -0,0 +1,18 @@
+namespace DuoClassLibrary.Exceptions
+{
+    public class QuizServiceProxyException : Exception
+    {
+        public QuizServiceProxyException()
+        {
+        }
+
+        public QuizServiceProxyException(string message) : base(message)
+        {
+        }
+
+        public QuizServiceProxyException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
