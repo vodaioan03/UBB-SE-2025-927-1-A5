9439d8e01975c7bec94b495783c43dea00fe88a0
dosqas <137123562+dosqas@users.noreply.github.com>
2025-05-12 06:26:39 +0300

    Update namespaces in CreateAssociationExerciseViewModel
    Refactor the `CreateAssociationExerciseViewModel.cs` file to replace `Duo.Models` and `Duo.Models.Exercises` with `DuoClassLibrary.Models.Exercises` and `DuoClassLibrary.Models`. This ensures the correct instantiation of the `AssociationExercise` class from the updated namespace.

--- Duo/ViewModels/CreateExerciseViewModels/CreateAssociationExerciseViewModel.cs
+++ Duo/ViewModels/CreateExerciseViewModels/CreateAssociationExerciseViewModel.cs
@@ -5,8 +5,8 @@ using System.Diagnostics;
 using System.Linq;
 using System.Windows.Input;
 using Duo.Commands;
-using Duo.Models;
-using Duo.Models.Exercises;
+using DuoClassLibrary.Models.Exercises;
+using DuoClassLibrary.Models;
 using Duo.ViewModels.Base;
 using Duo.ViewModels.ExerciseViewModels;
 
@@ -60,7 +60,7 @@ namespace Duo.ViewModels.CreateExerciseViewModels
         {
             try
             {
-                Exercise newExercise = new Models.Exercises.AssociationExercise(0, question, difficulty, GenerateAnswerList(LeftSideAnswers), GenerateAnswerList(RightSideAnswers));
+                Exercise newExercise = new DuoClassLibrary.Models.Exercises.AssociationExercise(0, question, difficulty, GenerateAnswerList(LeftSideAnswers), GenerateAnswerList(RightSideAnswers));
                 return newExercise;
             }
             catch (Exception ex)
