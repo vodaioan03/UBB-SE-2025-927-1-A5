6da8d409fdef84349b9deb6bee0cb22784d6964f
dosqas <137123562+dosqas@users.noreply.github.com>
2025-05-12 06:30:10 +0300

    Enhance JSON serialization and validation logic
    - Added methods for serializing and deserializing `Exam` and `Quiz` objects in `JsonSerializationUtil.cs`, including support for associated exercises and a new method for exercise lists.
    - Implemented `RealDispatcherTimer` in `RealDispatcherTimer.cs` to provide a cross-platform timer solution with event notifications.
    - Improved validation logic in `ValidationHelper.cs` to enforce rules for sections, exams, quizzes, and exercises, ensuring data integrity and clearer error messages.

---
+++ DuoClassLibrary/Helpers/RealDispatcherTimer.cs
@@ -0,0 +1,80 @@
+﻿#pragma warning disable IDE0290
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Timers;
+using DuoClassLibrary.Interfaces.Helpers;
+
+namespace DuoClassLibrary.Helpers
+{
+    /// <summary>
+    /// Wrapper class for a cross-platform timer that implements our custom IDispatcherTimer interface.
+    /// This class uses System.Timers.Timer to provide timer functionality.
+    /// </summary>
+    [ExcludeFromCodeCoverage]
+    public class RealDispatcherTimer : IDispatcherTimer
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Initializes a new instance of the RealDispatcherTimer class.
+        /// </summary>
+        public RealDispatcherTimer()
+        {
+            timer = new System.Timers.Timer();
+            timer.Elapsed += OnElapsed;
+        }
+
+        #endregion
+
+        #region Fields
+
+        private readonly System.Timers.Timer timer;
+
+        #endregion
+
+        #region Events
+
+        /// <summary>
+        /// Event that occurs when the timer interval has elapsed.
+        /// </summary>
+        public event EventHandler<object> Tick;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the amount of time between timer ticks.
+        /// </summary>
+        public TimeSpan Interval
+        {
+            get => TimeSpan.FromMilliseconds(timer.Interval);
+            set => timer.Interval = value.TotalMilliseconds;
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Starts the timer.
+        /// </summary>
+        public void Start() => timer.Start();
+
+        /// <summary>
+        /// Stops the timer.
+        /// </summary>
+        public void Stop() => timer.Stop();
+
+        /// <summary>
+        /// Handles the Elapsed event of the timer and raises the Tick event.
+        /// </summary>
+        private void OnElapsed(object sender, ElapsedEventArgs e)
+        {
+            Tick?.Invoke(this, EventArgs.Empty);
+        }
+
+        #endregion
+    }
+}
