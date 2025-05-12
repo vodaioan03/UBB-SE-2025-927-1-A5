97276eb8adbff5a4724e31c8b3e687eb43ab2ab3
U.Abel <abelujfalusi4@gmail.com>
2025-05-12 21:11:38 +0300

    Created connection with Api in ViewCourses view (linked in controller through C#

--- Duo.Web/Program.cs
+++ Duo.Web/Program.cs
@@ -1,7 +1,19 @@
+using DuoClassLibrary.Services;
+using DuoClassLibrary.Services.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+var handler = new HttpClientHandler
+{
+    ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true
+};
+var httpClient = new HttpClient(handler);
+
+builder.Services.AddSingleton<HttpClient>();
+builder.Services.AddScoped<ICourseServiceProxy, CourseServiceProxy>();
+builder.Services.AddScoped<ICourseService, CourseService>();
 
 var app = builder.Build();
 
