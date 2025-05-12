5267d569787271d5f9ab5669aa71800fdfc81fbb
dosqas <137123562+dosqas@users.noreply.github.com>
2025-05-12 06:32:02 +0300

    Add core models for quizzes, courses, and user progress
    This commit introduces several new classes to enhance the application's data model:
    
    - `AnswerSubmission`: Represents user answers to quiz questions.
    - `Difficulty`: Enum for categorizing exercise or course difficulty levels.
    - `Enrollment`: Manages user enrollments in courses with relevant properties.
    - `Module`: Represents learning modules within courses, including metadata.
    - `Tag`: Categorizes courses or modules for better organization.
    - `User`: Represents platform users with properties for tracking progress.
    - `UserProgress`: Tracks user progress in modules with status and navigation properties.

---
+++ DuoClassLibrary/Models/Difficulty.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace DuoClassLibrary.Models
+{
+    /// <summary>
+    /// Represents the difficulty levels for exercises or courses.
+    /// </summary>
+    public enum Difficulty
+    {
+        /// <summary>
+        /// Represents an easy difficulty level.
+        /// </summary>
+        Easy = 1,
+
+        /// <summary>
+        /// Represents a normal difficulty level.
+        /// </summary>
+        Normal = 2,
+
+        /// <summary>
+        /// Represents a hard difficulty level.
+        /// </summary>
+        Hard = 3
+    }
+
+    /// <summary>
+    /// Provides a static list of difficulty levels as strings.
+    /// </summary>
+    [ExcludeFromCodeCoverage]
+    public static class DifficultyList
+    {
+        #region Fields
+
+        /// <summary>
+        /// A list of difficulty levels represented as strings.
+        /// </summary>
+        public static readonly List<string> Difficulties =
+        [
+            "Easy",
+            "Normal",
+            "Hard"
+        ];
+
+        #endregion
+    }
+}
---
+++ DuoClassLibrary/Models/UserProgress.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace DuoClassLibrary.Models
+{
+    [ExcludeFromCodeCoverage]
+    [Serializable]
+    public class UserProgress
+    {
+        public int UserId { get; set; }
+        public int ModuleId { get; set; }
+        public string Status { get; set; } = "not_completed";
+        public bool ImageClicked { get; set; } = false;
+
+        public User? User { get; set; }
+        public Module? Module { get; set; }
+    }
+}
