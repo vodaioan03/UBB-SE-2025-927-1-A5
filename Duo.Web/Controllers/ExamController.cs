f877cfed0e7e1a22f87afb2a34a068f6360707bb
Zsigmond Artur <arthurzsigmond7@gmail.com>
2025-05-12 17:14:09 +0300

    feat: added an exam controller

---
+++ Duo.Web/Controllers/ExamController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Duo.Web.Controllers
+{
+    public class ExamController : Controller
+    {
+        private readonly ILogger<ExamController> _logger;
+
+        public ExamController(ILogger<ExamController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult ManageExam()
+        {
+            return View("~/Views/Exam/ManageExam.cshtml");
+        }
+
+        public IActionResult AddExam()
+        {
+            return View("~/Views/Exam/AddExam.cshtml");
+        }
+    }
+}
