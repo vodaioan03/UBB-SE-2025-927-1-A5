1f9dc464bf0685aa5177c69a567a7b9385dce2eb
Toma Alexandru <alextoma1704@gmail.com>
2025-05-13 13:30:41 +0300

    feat: created the blank exercise components

---
+++ Duo.Web/Controllers/ExerciseController.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+using Duo.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Duo.Web.Controllers
+{
+    public class ExerciseController : Controller
+    {
+        private readonly ILogger<ExerciseController> _logger;
+        public ExerciseController(ILogger<ExerciseController> logger)
+        {
+            _logger = logger;
+        }
+        // GET: Exercise
+        // This is just a placeholder to show the types of exercise components from Views/Exercise
+
+        // Just show the types type of exercise components from Views/Exercise
+
+        public IActionResult Index()
+        {
+            // URL : /exercise
+            return View("~/Views/Exercise/AssociationExercise.cshtml");
+        }
+        public IActionResult AssociationExercise()
+        {
+            // URL : /exercise/AssociationExercise
+            return View("~/Views/Exercise/AssociationExercise.cshtml");
+        }
+        public IActionResult FillInTheBlank()
+        {
+            // URL : /exercise/FillInTheBlank
+            return View("~/Views/Exercise/FillInTheBlank.cshtml");
+        }
+        public IActionResult MultipleChoice()
+        {
+            // URL : /exercise/MultipleChoice
+            return View("~/Views/Exercise/MultipleChoice.cshtml");
+        }
+        public IActionResult FillBlanks()
+        {
+            // URL : /exercise/FillBlanks
+            return View("~/Views/Exercise/FillBlanks.cshtml");
+        }
+    }
+}
