d81d49b823e6345b62ec3c1aeb946ab4990d2827
iuliaszarics <iuliaszarics@gmail.com>
2025-05-13 12:29:02 +0300

    Add RoadmapViewModel class to manage roadmap data
    Introduces the `RoadmapViewModel` class in the `Duo.Web.Models` namespace.
    This class includes properties for `Name`, a list of `Sections`, and
    `NumberOfCompletedSections`. Necessary models from `DuoClassLibrary`
    are imported, and the `Sections` property is initialized with a new
    list instance.

---
+++ Duo.Web/Models/RoadmapViewModel.cs
@@ -0,0 +1,13 @@
+﻿using DuoClassLibrary.Models.Roadmap;
+using DuoClassLibrary.Models.Sections;
+using System.Collections.Generic;
+
+namespace Duo.Web.Models
+{
+    public class RoadmapViewModel
+    {
+        public string Name { get; set; }
+        public List<Section> Sections { get; set; } = new();
+        public int NumberOfCompletedSections { get; set; }
+    }
+}
