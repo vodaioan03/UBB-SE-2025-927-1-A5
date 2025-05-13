166294662bda5f55a9f4bd5cc25d5bf070e33721
Toma Alexandru <alextoma1704@gmail.com>
2025-05-13 15:49:15 +0300

    feat: Added a separate page to view each type of exercise

---
+++ Duo.Web/Models/Exercise/Exercise.cs
@@ -0,0 +1,9 @@
+﻿namespace Duo.Web.Models.Exercise
+{
+    public abstract class Exercise
+    {
+        public int ExerciseId { get; set; }
+        public string Type { get; set; }
+        public string Title { get; set; }
+    }
+}
