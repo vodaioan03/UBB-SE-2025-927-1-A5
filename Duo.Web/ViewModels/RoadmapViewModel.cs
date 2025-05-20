1758709762a30d3a889fad0c3cbe768973beeec6
dosqas <137123562+dosqas@users.noreply.github.com>
2025-05-20 13:14:36 +0300

    Add RoadmapViewModel with properties for roadmap data
    Introduced a new class `RoadmapViewModel` in the `Duo.Web.ViewModels` namespace. This class includes a `Roadmap` property of type `Roadmap` and a `Sections` property of type `List<SectionViewModel>`. Added necessary using directives for `DuoClassLibrary.Models.Roadmap` and `Microsoft.AspNetCore.Mvc`.

---
+++ Duo.Web/ViewModels/RoadmapViewModel.cs
@@ -0,0 +1,11 @@
+﻿using DuoClassLibrary.Models.Roadmap;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Duo.Web.ViewModels
+{
+    public class RoadmapViewModel
+    {
+        public Roadmap Roadmap { get; set; }
+        public List<SectionViewModel> Sections { get; set; }
+    }
+}
