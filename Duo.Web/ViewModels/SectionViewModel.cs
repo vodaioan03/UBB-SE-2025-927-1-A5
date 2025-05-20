63d1d796ae1641dd2c2f397584dd16bd6b646bbb
dosqas <137123562+dosqas@users.noreply.github.com>
2025-05-20 13:14:44 +0300

    Add SectionViewModel class for section management
    Introduces the `SectionViewModel` class in the `Duo.Web.ViewModels` namespace. This class includes properties such as `Section`, `IsCompleted`, `IsUnlocked`, `CompletedQuizzes`, `QuizButtons`, and `ExamButton`, facilitating the encapsulation of section-related state and behavior for the user interface.

---
+++ Duo.Web/ViewModels/SectionViewModel.cs
@@ -0,0 +1,14 @@
+﻿using DuoClassLibrary.Models.Sections;
+
+namespace Duo.Web.ViewModels
+{
+    public class SectionViewModel
+    {
+        public Section Section { get; set; }
+        public bool IsCompleted { get; set; }
+        public bool IsUnlocked { get; set; }
+        public int CompletedQuizzes { get; set; }
+        public List<QuizButtonViewModel> QuizButtons { get; set; }
+        public QuizButtonViewModel ExamButton { get; set; }
+    }
+}
