a4bb584cbe884cfb183d219dc1465c5bdfa36068
Andreea410 <andreeaungur494@yahoo.com>
2025-05-21 14:10:43 +0300

    Feat: Fixed the create exercises forms

---
+++ Duo.Web/Views/Exercise/_AssociationExerciseCreateForm.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Duo.Web.Views.Exercise
+{
+    public class _AssociationExerciseCreateFormModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Duo.Web/Views/Exercise/_FillInTheBlankExerciseCreateForm.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Duo.Web.Views.Exercise
+{
+    public class _FillInTheBlankExerciseCreateFormModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
